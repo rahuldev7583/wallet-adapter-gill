11b44447c7e3be97837502bbd94eb74a5c5d17bf
rahuldev7583 <rahuldev7583@gmail.com>
2025-09-18 04:20:22 +0530

    added disconnect

--- app/WalletPage.tsx
+++ app/WalletPage.tsx
@@ -5,7 +5,8 @@ import { useWallet } from './WalletContext';
 import { useBalance } from '@gillsdk/react';
 
 const WalletPage = () => {
-  const { account, wallet } = useWallet();
+  const { account, wallet, connectedWallet, isConnected, setConnectedWallet } =
+    useWallet();
   const { balance } = useBalance({
     address: account?.address,
   });
@@ -17,6 +18,12 @@ const WalletPage = () => {
   return (
     wallet && (
       <div className='ml-10 mt-4'>
+        <button
+          className='bg-black text-white px-3 py-2 rounded-xl  ml-[40%] mt-8'
+          onClick={() => setConnectedWallet(null)}
+        >
+          Disconnect
+        </button>
         <h1 className='text-xl mt-8'>
           Connected to
           <span className='font-bold'> {account?.address}</span>
